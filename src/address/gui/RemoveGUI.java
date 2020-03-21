6b3ed9c44e224c12a2d43de56f8a22ba764551a2
Jessica Asuncion <jessica.asuncion4@yahoo.com>
2020-03-20 20:18:32 -0700

    Update RemoveGUI files

---
+++ src/address/gui/RemoveGUI.java
@@ -0,0 +1,36 @@
+package address.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RemoveGUI {
+
+    private JFrame frmClass;
+
+    public static void NewScreen()
+    {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    RemoveGUI window = new RemoveGUI();
+                    window.frmClass.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public RemoveGUI() {
+        initialize();
+    }
+
+    private void initialize()
+    {
+        frmClass = new JFrame("New Address Entry");
+        frmClass.setBounds(600, 600, 600, 600);
+        frmClass.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmClass.setLayout(new FlowLayout());
+    }
+}
